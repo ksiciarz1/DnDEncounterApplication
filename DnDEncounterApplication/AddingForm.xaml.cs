6d3a654aef79e327c4e74172cd7c4d8bd1b4fa6f
ksiciarz1 <kacpersiciarz2001@gmail.com>
2022-12-25 01:02:05 +0100

    Added the ability to add attack, spells and weapons*. Weapons are as just a name.

--- DnDEncounterApplication/AddingForm.xaml.cs
+++ DnDEncounterApplication/AddingForm.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Numerics;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
 namespace DnDEncounterApplication
 {
@@ -13,10 +17,47 @@ namespace DnDEncounterApplication
     /// </summary>
     public partial class AddingForm : Page
     {
+        private TextBox[] textBoxes;
+
+        private Dictionary<string, string> attacks = new Dictionary<string, string>();
+        private Dictionary<string, string> spells = new Dictionary<string, string>();
+        private Dictionary<string, string> weapons = new Dictionary<string, string>();
+
         public AddingForm()
         {
             InitializeComponent();
             PlayerRadio.IsChecked = true;
+
+            textBoxes = new TextBox[]
+            {
+                HPTextBox,
+                LVLTextBox,
+                ACTextBox,
+                CRTextBox,
+                EXPTextBox,
+                ProficencyBonusTextBox,
+                AttackBonusTextBox,
+                SaveDCTextBox,
+                SpellAttackTextBox
+            };
+
+            foreach (TextBox textBox in textBoxes)
+            {
+                textBox.TextChanged += TextBox_TextChanged;
+            }
+        }
+
+        /// <summary>
+        /// Used for validating
+        /// </summary>
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            Regex regex = new Regex("^[0-9]+$", RegexOptions.IgnoreCase);
+            if (!regex.IsMatch(textBox.Text))
+                textBox.BorderBrush = new SolidColorBrush(Color.FromRgb(255, 40, 40));
+            else
+                textBox.BorderBrush = null;
         }
 
         private void EnemyRadio_Checked(object sender, System.Windows.RoutedEventArgs e)
@@ -44,7 +85,12 @@ namespace DnDEncounterApplication
 
         private async void AddPlayer(bool saveToFile)
         {
-            // TODO: Validation
+            // TODO: Make adding weapons better
+            List<Weapon> trueWeapons = new List<Weapon>();
+            foreach (var weapon in weapons)
+                trueWeapons.Add(new Weapon() { Name = weapon.Key });
+
+
             PlayerCharacter player = new PlayerCharacter()
             {
                 Name = NameTextBox.Text,
@@ -54,7 +100,10 @@ namespace DnDEncounterApplication
                 ProficencyBonus = Convert.ToInt32(ProficencyBonusTextBox.Text),
                 AttackBonus = Convert.ToInt32(AttackBonusTextBox.Text),
                 SpellAttack = Convert.ToInt32(SpellAttackTextBox.Text),
-                SaveDC = Convert.ToInt32(SaveDCTextBox.Text)
+                SaveDC = Convert.ToInt32(SaveDCTextBox.Text),
+                Attacks = attacks,
+                Spells = spells,
+                Weapons = trueWeapons
             };
 
             AddingFormWindow parentWindow = Window.GetWindow(this) as AddingFormWindow;
@@ -77,7 +126,6 @@ namespace DnDEncounterApplication
         }
         private async void AddEnemy(bool saveToFile)
         {
-            // TODO: Validation
             Enemy enemy = new Enemy()
             {
                 Name = NameTextBox.Text,
@@ -87,7 +135,8 @@ namespace DnDEncounterApplication
                 EXP = Convert.ToInt32(EXPTextBox.Text),
                 ProficencyBonus = Convert.ToInt32(ProficencyBonusTextBox.Text),
                 AttackBonus = Convert.ToInt32(AttackBonusTextBox.Text),
-                SaveDC = Convert.ToInt32(SaveDCTextBox.Text)
+                SaveDC = Convert.ToInt32(SaveDCTextBox.Text),
+                Attacks = attacks
             };
 
             AddingFormWindow parentWindow = Window.GetWindow(this) as AddingFormWindow;
@@ -118,6 +167,10 @@ namespace DnDEncounterApplication
             AttackBonusTextBox.Text = "";
             SaveDCTextBox.Text = "";
             SpellAttackTextBox.Text = "";
+
+            attacks.Clear();
+            spells.Clear();
+            weapons.Clear();
         }
         private string GetPath(string name) => name.Trim().Replace(" ", "_") + ".json";
 
@@ -133,5 +186,34 @@ namespace DnDEncounterApplication
         private void CancelButton_Click(object sender, System.Windows.RoutedEventArgs e) => CloseWindow();
 
         private void CloseWindow() => Window.GetWindow(this).Close();
+
+        private void AddSpecialButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button == AddAttackButton)
+                new Special(Special.Type.Attack, this);
+            else if (button == AddSpellButton)
+                new Special(Special.Type.Spell, this);
+            else if (button == AddWeaponButton)
+                new Special(Special.Type.Weapon, this);
+        }
+
+        internal void AddSpecial(Special.Type type, string name, string description)
+        {
+            // TODO: Make adding weapons better
+            switch (type)
+            {
+                case Special.Type.Attack:
+                    attacks.Add(name, description);
+                    break;
+                case Special.Type.Spell:
+                    spells.Add(name, description);
+                    break;
+                case Special.Type.Weapon:
+                    weapons.Add(name, description);
+                    break;
+            }
+        }
     }
 }
---
+++ DnDEncounterApplication/Special.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DnDEncounterApplication
+{
+    /// <summary>
+    /// Interaction logic for Special.xaml
+    /// </summary>
+    public partial class Special : Window
+    {
+        private readonly AddingForm Parent;
+        private Type type;
+        public Special(Type type, AddingForm parent)
+        {
+            InitializeComponent();
+            Parent = parent;
+            this.type = type;
+
+            switch (type)
+            {
+                case Type.Attack:
+                    break;
+                case Type.Spell:
+                    break;
+                case Type.Weapon:
+                    break;
+            }
+        }
+
+        public enum Type
+        {
+            Attack,
+            Spell,
+            Weapon
+        }
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameTextBox.Text != "")
+            {
+                Parent.AddSpecial(type, NameTextBox.Text, DescTextBox.Text);
+                Close();
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
